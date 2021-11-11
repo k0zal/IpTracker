43548e3d43e58bf3a1c9db98b519a7c4b11a9e9c
k0zal <69092737+k0zal@users.noreply.github.com>
2021-11-11 01:28:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import Header from './Header'
 import Information from './Information'
 import Map from './Map'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
      <Header />
